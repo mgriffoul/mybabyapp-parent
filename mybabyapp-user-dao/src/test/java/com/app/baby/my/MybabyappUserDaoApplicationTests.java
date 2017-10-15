07b3dca77462a17b81283f26da758e65655fab21
mathieu_griffoul <mgriffoul@voyages-sncf.com>
2017-10-15 18:07:25 +0200

    modif des POM

---
+++ mybabyapp-user-dao/src/test/java/com/app/baby/my/MybabyappUserDaoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.app.baby.my;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MybabyappUserDaoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
