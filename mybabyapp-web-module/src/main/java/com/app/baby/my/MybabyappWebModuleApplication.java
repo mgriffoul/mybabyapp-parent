40452041034febcf2f5134a025a5c96b8fbe2daa
mathieu_griffoul <mgriffoul@voyages-sncf.com>
2017-10-28 13:11:22 +0200

    Changement nom de la base de donnée + adaptation configuration

--- mybabyapp-web-module/src/main/java/com/app/baby/my/MybabyappWebModuleApplication.java
+++ mybabyapp-web-module/src/main/java/com/app/baby/my/MybabyappWebModuleApplication.java
@@ -2,11 +2,26 @@ package com.app.baby.my;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.config.AbstractMongoConfiguration;
+import com.mongodb.Mongo;
+import com.mongodb.MongoClient;
 
 @SpringBootApplication
-public class MybabyappWebModuleApplication {
+public class MybabyappWebModuleApplication extends AbstractMongoConfiguration{
+
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(MybabyappWebModuleApplication.class, args);
+	}
+
+	@Override
+	protected String getDatabaseName() {
+		return "babyappdb";
+	}
+
+	@Override
+	public Mongo mongo() throws Exception {
+		return new MongoClient("localhost", 27017);
 	}
 }
